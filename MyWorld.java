7b418de33f3feee696ef68c6c1ae17944f03eccf
Timo van Zuijlen <302346941@student.rocmondriaan.nl>
2018-11-13 09:28:09 +0100

    enemy now snail

--- MyWorld.java
+++ MyWorld.java
@@ -54,7 +54,7 @@ public class MyWorld extends World {
         // Alle objecten toevoegen aan de wereld: camera, main karakter en mogelijke enemies
         addObject(camera, 0, 0);
         addObject(hero, 300, 200);
-        addObject(new Enemy(), 1170, 590);
+        addObject(new Enemy(), 1170, 645);
         
         // Initialiseren van de CollisionEngine zodat de speler niet door de tile heen kan lopen.
         // De collision engine kijkt alleen naar de tiles die de variabele solid op true hebben staan.
